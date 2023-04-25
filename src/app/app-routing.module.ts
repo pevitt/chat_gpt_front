8b019af0c9b4e1275ae3f5f256695890361096cf
pevitt <jrigoberto17@gmail.com>
2023-04-24 20:55:51 -0500

    add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedGuard } from './core/guards/logged.guard';
+import { UnloggedGuard } from './core/guards/unlogged.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [LoggedGuard],
     loadChildren: () =>
       import('./presentation/features/home/home.module').then(
         (m) => m.HomeModule
@@ -11,6 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'auth',
+    canActivate: [UnloggedGuard],
     loadChildren: () =>
       import('./presentation/features/auth/auth.module').then(
         (m) => m.AuthModule
---
+++ src/app/core/guards/logged.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, createUrlTreeFromSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const token = localStorage.getItem('adviser.token');
+    return token ? true : createUrlTreeFromSnapshot(route, ['/auth/login']);
+  }
+  
+}
